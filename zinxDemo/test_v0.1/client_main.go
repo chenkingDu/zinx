b26841af1b24623f563aca4ae51d249a82815c35
kkkkking233 <759846419@qq.com>
2019-05-22 17:23:01 +0800

    v0.1的测试

---
+++ zinxDemo/test_v0.1/client_main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net"
+	"fmt"
+	"time"
+)
+
+func main(){
+	conn ,err := net.Dial("tcp","0.0.0.0:8999")
+	if err != nil{
+		fmt.Println("Client Dial err: ",err)
+		return
+	}
+
+	for {
+		_,err := conn.Write([]byte("zinx client come in!"))
+		if err != nil{
+			fmt.Println("Client Write err: ",err)
+			return
+		}
+		buf := make([]byte,512)
+		n,err := conn.Read(buf)
+		if err != nil{
+			fmt.Println("Client Read err: ",err)
+			return
+		}
+		fmt.Printf(" servar call back : %s, cnt = %d\n", buf,n)
+
+		time.Sleep(time.Second)
+	}
+
+}
---
+++ zinxDemo/test_v0.1/server_main.go
@@ -0,0 +1,9 @@
+package main
+
+import "zinx/zinx/znet"
+
+func main() {
+	s := znet.NewServer("zinx v0.1")
+
+	s.Serve()
+}
